01d7e3bfe47045fcd8bfad89b313ace919b9f9d9
zhonggaorong <458673541@qq.com>
2016-11-24 15:54:18 +0800

    Initial Commit

---
+++ AppStoreHorizontallyUICollectionVIew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppStoreHorizontallyUICollectionVIew
+//
+//  Created by 张国荣 on 16/11/24.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
